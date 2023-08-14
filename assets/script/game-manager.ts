3f72ab8a8ba29abc71e0139685a010040476cf9a
yushuyun <shuyun.yu@cocos.com>
2023-08-14 16:23:20 +0800

    feat init project

---
+++ assets/script/game-manager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameManager')
+export class GameManager extends Component {
+    start () {
+
+    }
+
+    update (deltaTime: number) {
+
+    }
+}
+
